9f1e0be626aae0ce84aed015953bac6b474b3aa2
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2023-09-26 01:31:45 +0100

    Reaction Timer pt1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>  
+    <h1>Reaction Timer Game</h1>
+    <h3> Test your reaction speed!</h3>
+    <button @click="startgame" :disabled="isPlaying" >Play!</button>
+
+</div>
+<div>   
+  <Block v-if="isPlaying" :delay="delay"/>
+</div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block},
+  data () {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    startgame() {
+      this.isPlaying = !this.isPlaying
+      this.timer()
+      console.log(this.delay)    
+    },
+    timer() {
+      this.delay = 1000 + Math.random() * 8000
+
+    }
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="block" v-if="showBlock">
+    CLICK ME FAST
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay', 'isPlaying'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        setTimeout(()=>{this.showBlock = true}, this.delay)
+
+    }
+
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: greenyellow;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
